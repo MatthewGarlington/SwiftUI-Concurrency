825dc84d2e110eada5b625e9d90e26b1032b0101
Matthew Garlington <sbmatthewg@gmail.com>
2021-11-26 22:22:33 -0500

    Initial Commit

---
+++ SwiftUIConcurrencyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIConcurrencyApp
+//
+//  Created by us-guest on 11/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIConcurrencyApp/SwiftUIConcurrencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIConcurrencyAppApp.swift
+//  SwiftUIConcurrencyApp
+//
+//  Created by us-guest on 11/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIConcurrencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
